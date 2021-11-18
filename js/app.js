c12fc32cedde2be5d3f1425c322d80eb6b886709
Test Projects 83 <testprojects83@gmail.com>
2021-11-18 17:39:05 +0000

    updated

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js';
+// import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js';
 
-const startBtn = document.getElementById('start_btn');
+// const startBtn = document.getElementById('start_btn');
 
-startBtn.addEventListener('click', () => {
-  if (user) {
+// startBtn.addEventListener('click', () => {
+//   if (user) {
+//     window.location.href = './home.html';
+//   } else {
+//     window.location.href = './login.html';
+//   }
+// });
+
+// // window.navigator.onLine()
+
+// const darkmode = false;
+
+// if (!darkmode) {
+//   const body = document.getElementsByTagName('body');
+
+//   body.classList.add('lightmode');
+// } else {
+//   body.classList.remove('lightmode');
+//   body.classList.add('darkmode');
+// }
+
+let startbtn = document.getElementById('start');
+
+const authStateChanged = false;
+
+startbtn.addEventListener('click', () => {
+  if (authStateChanged) {
     window.location.href = './home.html';
   } else {
-    window.location.href = './login.html';
+    window.location.href = '.../auth/signin.html';
   }
 });
-
-// window.navigator.onLine()
-
-const darkmode = false;
-
-if (!darkmode) {
-  const body = document.getElementsByTagName('body');
-
-  body.classList.add('lightmode');
-} else {
-  body.classList.remove('lightmode');
-  body.classList.add('darkmode');
-}
